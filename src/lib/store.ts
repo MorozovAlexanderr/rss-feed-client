a7846f5bcec7fd820be5046fb4ce5b23cd37d2cf
MorozovAlexanderr <sashamorozov309@gmail.com>
2023-05-04 17:04:43 +0300

    persist user

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,14 +1,38 @@
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { authApi } from '@/features/auth/api';
 import { authReducer } from '@/features/auth/slice';
-import { configureStore } from '@reduxjs/toolkit';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['api'],
+};
+
+const reducer = combineReducers({
+  [authApi.reducerPath]: authApi.reducer,
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(authApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -2,15 +2,23 @@ import { store } from '@/lib/store';
 import React from 'react';
 import { Provider as StoreProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
+const persistor = persistStore(store);
+
 const AppProvider = ({ children }: AppProviderProps): JSX.Element => {
   return (
     <BrowserRouter>
-      <StoreProvider store={store}>{children}</StoreProvider>
+      <StoreProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          {children}
+        </PersistGate>
+      </StoreProvider>
     </BrowserRouter>
   );
 };
